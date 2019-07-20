98e525e919128eb355a162b50174df720ffa1e4b
Demo <demotascha@gmail.com>
2019-07-20 13:16:58 +0800

    docs: 用來定期抓取資料

---
+++ fetchbot.py
@@ -0,0 +1,6 @@
+import json
+import requests
+
+results = requests.get("https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=59629791-5f4f-4c91-903b-e9ab9aa0653b").json()
+with open('data.json', 'w') as outfile:  
+    json.dump(results, outfile, ensure_ascii=False)
