b6877ae3ac835df234a53fbebe5ce176136b7db1
Demo <demotascha@gmail.com>
2019-08-25 16:38:45 +0800

    fix: remove useless files

--- fetchbot.py
+++
@@ -1,6 +0,0 @@
-import json
-import requests
-
-results = requests.get("https://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=59629791-5f4f-4c91-903b-e9ab9aa0653b").json()
-with open('data.json', 'w') as outfile:
-    json.dump(results, outfile, ensure_ascii=False)
